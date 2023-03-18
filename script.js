fd53ad4b2581ca24faddefe6d7a02b7dd73927df
ryotaro-0805 <rasouryo@yahoo.co.jp>
2023-03-18 23:09:58 +0900

    ✅ 進めました

---
+++ script.js
@@ -0,0 +1,13 @@
+const select=document.getElementById('shop_select');
+const input=document.getElementById('shop_input');
+
+
+
+
+input.addEventListener('click',()=>{
+    if (input.checked){
+        select.disabled=false;
+    } else {
+        select.disabled=true;
+    }
+});
